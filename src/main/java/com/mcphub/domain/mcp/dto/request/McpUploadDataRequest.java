23199eee64b05319e82227d43bcde9e863f19428
js2000 <js2000@sbsystems.co.kr>
2025-09-18 19:07:04 +0900

    [chore] remove global folder to replace with submodule

--- src/main/java/com/mcphub/domain/mcp/dto/request/McpUploadDataRequest.java
+++ src/main/java/com/mcphub/domain/mcp/dto/request/McpUploadDataRequest.java
@@ -4,23 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class McpMetaDataRequest {
 
 	private String name;
-	private String version;
+	//private String version;
 	private String description;
-	private String imageUrl;
-	private Boolean isKeyRequired;
 	private Long categoryId;
-	private Long platformId;
-	private Long licenseId;
 	private String sourceUrl;
+	private String imageUrl;
+	//private String version;
+	//플랫폼 이름으로 서치
+	//private Long platformId;
+	private String platformName;
+	// 이미지 파일로 받을 예정
+	// private String imageUrl;
+	private String developerName;
+	private Boolean isKeyRequired;
+	private Long licenseId;
 	private List<McpToolRequest> tools;
 }
