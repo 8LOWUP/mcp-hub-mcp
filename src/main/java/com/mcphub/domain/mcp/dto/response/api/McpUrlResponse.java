6b17ccb9571d56631ac05e24c6ae3dfb2a233e4a
js2000 <js2000@sbsystems.co.kr>
2025-09-19 18:08:28 +0900

    [add] 로컬 이미지 저장 경로

---
+++ src/main/java/com/mcphub/domain/mcp/dto/response/api/McpUrlResponse.java
@@ -0,0 +1,15 @@
+package com.mcphub.domain.mcp.dto.response.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class McpUrlResponse {
+	private Long mcpId;
+	private String url;
+}
