2cb894cb021bbebe48d6fb86185dc71cce7fc21b
js2000 <js2000@sbsystems.co.kr>
2025-10-17 16:17:28 +0900

    [fix] : add elasticsearch for searching mcp.

--- src/main/java/com/mcphub/domain/mcp/repository/elasticsearch/McpElasticsearchRepository.java
+++ src/main/java/com/mcphub/domain/mcp/repository/elasticsearch/McpElasticsearchRepository.java
@@ -1,7 +1,8 @@
 package com.mcphub.domain.mcp.repository.elasticsearch;
 
-import com.mcphub.domain.mcp.entity.McpVector;
+import com.mcphub.domain.mcp.entity.McpElasticsearch;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface McpElasticsearchRepository extends ElasticsearchRepository<McpVector, String> {
+public interface McpElasticsearchRepository extends ElasticsearchRepository<McpElasticsearch, String> {
+
 }
